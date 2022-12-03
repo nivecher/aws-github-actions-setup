3b8984eec6afec30675df138c7f4bb0535eb4815
Morgan Davis <mtd37@hotmail.com>
2022-12-03 11:42:49 -0600

    Control bootstrap updates

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,3 +6,7 @@ aws cloudformation deploy \
   --stack-name github-actions-setup \
   --template-file cloudformation/setup.yml \
   --capabilities CAPABILITY_NAMED_IAM
+
+# Run this to destroy
+# aws cloudformation delete-stack --stack-name github-actions-setup
+# aws cloudformation wait stack-delete-complete --stack-name github-actions-setup 
